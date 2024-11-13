b743aed3282fd6acdd03d2d0608b346bea950018
bng16 <biswajitnag161619@gmail.com>
2024-11-13 12:46:33 -0900

    completed user controller added some middlewares and somme utils

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -2,6 +2,8 @@
 import { User } from "./../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from './../utils/cloudinary.js';
 
 // Function to register user
 export const register = async (req, res) => {
@@ -72,7 +74,7 @@ export const login = async (req, res) => {
     // accessing the user info from User with email
     let user = await User.findOne({ email });
 
-    // checking if the user is available or not 
+    // checking if the user is available or not
     if (!user) {
       return res.status(401).json({
         Message: "Incorrect email or password",
@@ -117,7 +119,7 @@ export const login = async (req, res) => {
 
 
 // Logic for logout
-const logout= (_,res)=>{
+export const logout= (_,res)=>{
   try {
     return res.cookie("token",'',{maxAge:0}).json({
       message:`logout successfully`,
@@ -129,3 +131,116 @@ const logout= (_,res)=>{
 }
 
 
+// to get the user profile with id
+export const getProfile = async (req,res)=>{
+  try {
+    const userId = req.params.id;
+    let user= await User.findById(userId);
+    return res.status(200).json({
+      user,
+      success:true,
+    })
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+// edit profile
+export const editProfile = async (req,res)=>{
+  try {
+    const userId=req.id;
+    const {bio,gender}=req.body;
+    const profilePicture=req.file;
+    let cloudResponse;
+    
+    if(profilePicture){{
+      const fileUri=getDataUri(profilePicture);//this is to convert the image into uri (../utils/datauri.js)
+      cloudResponse=await cloudinary.uploader.upload(fileUri);//the uri file is being uploaded to cloudinary and cloudinary provide a link (../utils/cloudinary.js)<-cloudinary setup
+    }}
+
+
+    const user=await User.findById(userId);
+    if(!user){
+      return res.status(401).json({
+        message:"user not found!",
+        success:false,
+      })
+    }
+    if(bio) user.bio=bio;
+    if(gender) user.gender=gender;
+    if(profilePicture) user.profilePicture=cloudResponse.secure_url;
+
+    await user.save();
+
+    return res.status(200).json({
+      message:"Profile updated.",
+        success:true,
+        user,
+    })
+
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+
+// for suggesting users
+export const getSuggestedUser=async (req,res)=>{
+  try {
+    const suggestedUser=await User.findById({_id:{$ne:req.id}}).select("-password");
+    if(!suggestedUser){
+      return res.status(400).json({
+        message:"Currently do not have any users",
+      })
+    }
+    return res.status(200).json({
+      success:true,
+      users:suggestedUser,
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+// logic to follow and unfollow users
+export const followOrUnfollow= async (req,res)=>{
+  try {
+    const firstUser=req.id;   // this is the id of the user who is login and clicked follow or un-follow button!
+    const secondUser=req.params.id;   // this is the receiver user who is being followed or un-followed!
+    if(firstUser===secondUser){
+      return res.status(400).json({
+        message: "You can't follow yourself!",
+        success:false,
+      })
+    }
+    const user=User.findById(firstUser);
+    const targetUser=User.findById(secondUser);
+    if(!user || !targetUser){
+      return res.status(400).json({
+        message:"user not found!",
+        success:false,
+      })
+    }
+    const isFollowing=user.following.includes(secondUser);
+    if(isFollowing){
+      //unFollow logic
+      await Promise.all([
+        User.updateOne({_id:firstUser},{$pull:{following:secondUser}}),
+        User.updateOne({_id:secondUser},{$pull:{followers:firstUser}}),
+      ])
+      return res.status(200).json({message:"unfollowed successfully",success:true})
+    }else{
+      //follow logic
+      await Promise.all([
+        User.updateOne({_id:firstUser},{$push:{following:secondUser}}),
+        User.updateOne({_id:secondUser},{$push:{followers:firstUser}}),
+      ])
+      return res.status(200).json({message:"followed successfully",success:true})
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
